8d379109b2aca7aa4e1c363639a1a4c42adee187
sergio peralta <sergio.peralta@rhythmagency.com>
2023-07-18 18:56:13 -0600

    se agrego una libreria adicional

--- WebApplication2/WebApplication2/Program.cs
+++ WebApplication2/WebApplication2/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<CustomerDbContext>(option => option.options.UseInMemoryDatabase("ContactsDb"));
+//builder.Services.AddDbContext<CustomerDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
 builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
