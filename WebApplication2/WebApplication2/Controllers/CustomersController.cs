7569c2433e291f023e5008da30d889cefb56472c
sergio peralta <sergio.peralta@rhythmagency.com>
2023-07-18 18:28:55 -0600

    se agrego la ruta de actualizar y eliminar

--- WebApplication2/WebApplication2/Controllers/CustomersController.cs
+++ WebApplication2/WebApplication2/Controllers/CustomersController.cs
@@ -6,7 +6,7 @@ namespace WebApplication2.Controllers
     //localhost:4545/api/customers
     [ApiController]
     [Route("api/[Controller]")]
-    public class CustomersController: ControllerBase
+    public class CustomersController : ControllerBase
     {
         private readonly CustomerDbContext _context;
         public CustomersController(CustomerDbContext context)
@@ -23,13 +23,13 @@ namespace WebApplication2.Controllers
             return Ok(customers);
         }
 
-        [HttpGet]
+        [HttpGet("{id}")]
         public ActionResult<Customer> GetCustomer(int id)
         {
             //select top 1 a.* from customer a where customerId = id
             var customer = _context.Customers.Find(id);
 
-            if(customer == null)
+            if (customer == null)
             {
                 return NotFound();
             }
@@ -48,10 +48,38 @@ namespace WebApplication2.Controllers
                 //commit
                 _context.SaveChanges();
                 return Ok(customer);
-            } catch(Exception ex)
+            } catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCustomer(int id, Customer customer) 
+        { 
+            //update customer set id = 14, name = 'Sergio Peralta'
+            if(id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
