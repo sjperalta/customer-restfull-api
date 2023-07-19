b9520ae5e02f322571d062d31222bf3db7e35c0e
sergio peralta <sergio.peralta@rhythmagency.com>
2023-07-18 18:11:32 -0600

    proyecto inicial

---
+++ WebApplication2/WebApplication2/CustomerDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
